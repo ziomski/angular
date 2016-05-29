da64ff36f367ec42932290cd38fa757f9c9e1fab
ziomski <ziomski@gmail.com>
2016-05-29 18:36:02 +0200

    Dodanie uslugi pobierania danych

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,6 +1,8 @@
 import { Hero } from './hero';
 import { HeroDetailComponent } from './hero-detail.component';
-import { Component } from '@angular/core';
+import { HeroService } from './hero.service';
+import { Component, OnInit } from '@angular/core';
+
 @Component({
   selector: 'my-app',
     template:`
@@ -64,25 +66,24 @@ import { Component } from '@angular/core';
       border-radius: 4px 0 0 4px;
     }
   `],
-  directives: [HeroDetailComponent]
+  directives: [HeroDetailComponent],
+  providers: [HeroService]
 })
 
 export class AppComponent {
 	title = 'Tour of Heroes';
-	public heroes = HEROES;
+	heroes: Hero[];
 	selectedHero: Hero;
+
+	constructor(private heroService: HeroService) { }
+
+ 	getHeroes() {
+    	this.heroService.getHeroes().then(heroes => this.heroes = heroes);
+  	}
+	
+	ngOnInit() {
+    	this.getHeroes();
+  	}
+
 	onSelect(hero: Hero) { this.selectedHero = hero; }
 }
-
-var HEROES: Hero[] = [
-  { "id": 11, "name": "Mr. Nice" },
-  { "id": 12, "name": "Narco" },
-  { "id": 13, "name": "Bombasto" },
-  { "id": 14, "name": "Celeritas" },
-  { "id": 15, "name": "Magneta" },
-  { "id": 16, "name": "RubberMan" },
-  { "id": 17, "name": "Dynama" },
-  { "id": 18, "name": "Dr IQ" },
-  { "id": 19, "name": "Magma" },
-  { "id": 20, "name": "Tornado" }
-];
