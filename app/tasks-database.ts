b4962db9639527d064d19faa7c8cb1bbfdc18753
ziomski <ziomski@gmail.com>
2016-06-02 23:30:03 +0200

    Poprawki

--- app/tasks-database.ts
+++ app/tasks-database.ts
@@ -1,10 +1,8 @@
 import { Task } from './task';
 
 export var TASKS: Task[] = [
-  {id: 1, task_date: '2016-05-05', name: 'Zrobic zakupy', description: 'Kupic bulki', done: false},
-  {id: 2, task_date: '2016-05-06', name: 'Zrobic pranie', description: 'Zrobic biale pranie', done: true},
-  {id: 3, task_date: '2016-05-06', name: 'Zrobic pranie', description: 'Zrobic biale pranie', done: false},
-  {id: 4, task_date: '2016-05-06', name: 'Zrobic pranie', description: 'Zrobic biale pranie', done: false},
-  {id: 5, task_date: '2016-05-06', name: 'Zrobic pranie', description: 'Zrobic biale pranie', done: false},
-  {id: 6, task_date: '2016-05-06', name: 'Zrobic pranie', description: 'Zrobic biale pranie', done: false},
+  {id: 1, task_date: '01-06-2016', name: 'Zrobic zakupy', description: 'Kupic bulki i kabanosy', done: false},
+  {id: 2, task_date: '01-06-2016', name: 'Zrobic pranie', description: 'Zrobic biale pranie', done: false},
+  {id: 3, task_date: '02-06-2016', name: 'Oplacic rachunki', description: 'Oplacic zalegle rachunki za prad gaz itp', done: false},
+  {id: 4, task_date: '02-06-2016', name: 'Naprawic komputer', description: 'Zabrac laptopa od klienta i przeinstalowac system', done: true}
 ];
